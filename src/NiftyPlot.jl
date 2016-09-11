a790948de34c79f5a9043e3cf63f18a21c5df1f6
Kapil Singh Pawar <kpawar@gmail.com>
2016-09-11 23:53:46 +0530

    Populate Nifty Dataframe using Nifty data from github

--- src/NiftyPlot.jl
+++ src/NiftyPlot.jl
@@ -1,6 +1,7 @@
-using PyPlot, DataFrames
+using DataFrames, Requests, PyPlot
 
-function ma{T <: Real}(x::Vector{Float64}, wind::Int)
+# Function for backwards-looking Moving Average. Source: http://stackoverflow.com/questions/34406691/how-to-add-a-column-of-simple-moving-average-of-another-column-to-a-julia-data-f
+function ma(x::Vector{Float64}, wind::Int)
     len = length(x)
     y = Vector{Float64}(len)
     for i in 1:len
@@ -15,8 +16,13 @@ end
 
 fastmabars = 20;
 slowmabars = 50;
-df0 = readtable("C:/kapil/Nifty.csv", separator = ',', header = true) #_2015
-df = tail(df0, 1000)
+url = "https://raw.githubusercontent.com/kapilspawar/JuliaLearning/master/data/Nifty.csv"
+r = Requests.get_streaming(url; timeout = 30.0)
+if r.response.status / 100 != 2
+    error("Error downloading data")
+end
+df0 = readtable(r, separator = ',', header = true)
+df = tail(df0, 1000) # Restrict data to last 1000 days or approximately 4 years
 x = df[:Date]
 y1 = df[:Close]
 y2 = df[:Turnover_Rs_Cr_]
@@ -33,7 +39,7 @@ p = plot_date(dt, y1, color="blue", linestyle="-", marker="None", label="Close")
 
 pmafast = plot_date(dt, mafast, color="green", linestyle="-", marker="None", label=string(fastmabars, "d MA"))
 pmaslow = plot_date(dt, maslow, color="red", linestyle="-", marker="None", label=string(slowmabars, "d MA"))
-legend(handles=[p, pmafast, pmaslow], loc="upper left")
+legend(handles=[p; pmafast; pmaslow], loc="upper left")
 
 ax2 = ax1[:twinx]() # Create another axis on top of the current axis
 ax2[:set_ylim]([0, maximum(y2) * 5]) # Keep Volume bars to be no more than 20% of the total chart height
